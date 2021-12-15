dcb6cb36bd5594abc606d743829618700be4409f
Diana Selyuk <selukdiana@gmail.com>
2021-12-15 09:48:10 +0300

    add faker interface

---
+++ FakerInterfaces/FakerInterfaces/IGeneratorContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakerInterfaces
+{
+    public interface IGeneratorContext
+    {
+        Random Random { get; }
+        Type TargetType { get; }
+        IFaker Faker { get; }
+    }
+}
---
+++ FakerInterfaces/FakerInterfaces/IValueGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerInterfaces
+{
+    public interface IValueGenerator
+    {
+        object Generate(IGeneratorContext context);
+        bool CanGenerate(Type type);
+    }
+
+}
