708fac844896fcaa07d36a2a98c364317dc93f90
Diana Selyuk <selukdiana@gmail.com>
2021-12-15 09:44:44 +0300

    add program file to faker api

---
+++ FakerAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using FakerAPI.API;
+using FakerInterfaces;
+using System;
+
+namespace FakerAPI
+{
+    struct b {
+        public int a; 
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            IFaker faker = new Faker(); 
+            var a = faker.Create<User>();
+            Console.WriteLine(a);
+            var s = faker.Create<b>();
+            var test = faker.Create<Class>();
+            Console.WriteLine(test);
+            
+        }
+    }
+}
